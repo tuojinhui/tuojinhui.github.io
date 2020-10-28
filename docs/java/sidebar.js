11cda3b32a3acf4908b0d251d9eb9f5652af8acc
‘tuojinhui’ <1558238832@qq.com>
2020-10-28 11:32:25 +0800

    56 commit

--- docs/java/sidebar.js
+++ docs/java/sidebar.js
@@ -4,8 +4,13 @@ module.exports = [
         collapsable: true,
         children: [
             '/java/java1/knowledge.md',
-            '/java/java1/BigDecimals.md',
             '/java/java1/Executors.md',
+            '/java/java1/BigDecimals.md',
         ]
+    },
+    {
+        title: 'java侧边栏2',
+        collapsable: true,
+        children: ['/java/java2/']
     }
 ]
