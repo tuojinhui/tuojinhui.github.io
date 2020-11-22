8a93240d358e3c6ab8a267a301182b68f6966cef
‘tuojinhui’ <1558238832@qq.com>
2020-11-23 03:13:39 +0800

    72 commit

--- docs/.vuepress/components/Aqs.vue
+++ docs/.vuepress/components/Aqs.vue
@@ -8,14 +8,16 @@
 
         data() {
             return {
-                code: ''
+                code: '',
+                state: ''
             }
-
         },
 
         mounted() {
-            this.code = window.decodeURI(window.location.search.substr(1).match(new RegExp("(^|&)code=([^&]*)(&|$)", "i"))?.[2]);
+            this.code = window.decodeURIComponent(window.location.search.substr(1).match(new RegExp("(^|&)code=([^&]*)(&|$)", "i"))?.[2]);
+            this.state = window.decodeURIComponent(window.location.search.substr(1).match(new RegExp("(^|&)state=([^&]*)(&|$)", "i"))?.[2]);
             console.log('code ===', this.code)
+            console.log('state ===', this.state)
         },
 
         methods: {},
